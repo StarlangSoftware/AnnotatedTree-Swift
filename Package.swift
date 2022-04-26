b55270879a1ac17b075fb26e82a7c6da7b6c71d7
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-04-26 17:00:00 +0300

    Updated dependencies.

--- Package.swift
+++ Package.swift
@@ -12,16 +12,15 @@ let package = Package(
             targets: ["AnnotatedTree"]),
     ],
     dependencies: [
-        .package(name: "AnnotatedSentence", url: "https://github.com/StarlangSoftware/AnnotatedSentence-Swift.git", .exact("1.0.7")),
-        .package(name: "ParseTree", url: "https://github.com/StarlangSoftware/ParseTree-Swift.git", .exact("1.0.4")),
-        .package(name: "Util", url: "https://github.com/StarlangSoftware/Util-Swift.git", .exact("1.0.8")),
+        .package(name: "AnnotatedSentence", url: "https://github.com/StarlangSoftware/AnnotatedSentence-Swift.git", .exact("1.0.8")),
+        .package(name: "ParseTree", url: "https://github.com/StarlangSoftware/ParseTree-Swift.git", .exact("1.0.5"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AnnotatedTree",
-            dependencies: ["AnnotatedSentence", "ParseTree", "Util"]),
+            dependencies: ["AnnotatedSentence", "ParseTree"]),
         .testTarget(
             name: "AnnotatedTreeTests",
             dependencies: ["AnnotatedTree"]),
